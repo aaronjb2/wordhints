a525391b8866af5f0a414e555d7c68c0cffeba74
unknown <aaronjbraithwaite@gmail.com>
2019-02-05 00:13:45 -0700

    after installing everything for server side

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 require('dotenv').config();
+const massive = require('massive');
+const socket = require('socket.io');
 
 const app = express();
 
@@ -8,6 +10,13 @@ const {SERVER_PORT, MASSIVE_CONNECTION} = process.env;
 
 app.use( express.static( `${__dirname}/../build` ) );
 
-app.listen(SERVER_PORT,()=>{
-    console.log(`On the ${SERVER_PORT}th day of Christmas my true love gave to me..... nothing because I'm single.`);
+const io = socket(
+    app.listen(SERVER_PORT, () => {
+        console.log(`On the ${SERVER_PORT}th day of Christmas my true love gave to me..... nothing because I'm single`);
+    })
+)
+
+massive(MASSIVE_CONNECTION).then(db=> {
+    app.set('db', db);
+    console.log('db is connected');
 })
